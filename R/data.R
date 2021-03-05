e88e4f7c21ee7683fe7271fbf3a72d1ba2ef47de
Nick Williams <ntwilliams.personal@gmail.com>
2021-03-05 12:41:55 -0500

    draw from ordinal data.

--- R/data.R
+++ R/data.R
@@ -1,11 +1,12 @@
-box::use(here[here], data.table[setDT, copy], stats)
+box::use(here[here], data.table[setDT, copy], stats, teachingApps[rbeta4])
 
 #' @export
 generate_data <- function(data, type = c("survival", "binary", "ordinal"), 
                           prognostic = TRUE, seed, ...) {
   args <- list(...)
   switch(match.arg(type), 
-         survival = gds(data, args$n, args$effect_size, prognostic, seed))
+         survival = gds(data, args$n, args$effect_size, prognostic, seed), 
+         ordinal = gdo(data, args$n, args$effect_size, prognostic, seed))
 }
 
 gds <- function(data, n, effect_size, prognostic, seed) {
@@ -32,19 +33,70 @@ gds <- function(data, n, effect_size, prognostic, seed) {
   boot[]
 }
 
+plot_beta <- function(shape1, shape2) {
+  betar <- function(x) {
+    dbeta4(x, 0, 5, shape1, shape2)
+  }
+  curve(betar, 0, 5, 1000)
+}
+
+gdo <- function(data, n, effect_size, prognostic, seed) {
+  set.seed(seed)
+  if (prognostic) {
+    boot <- setDT(data[sample(nrow(data), n, replace = TRUE), ])
+  } else {
+    hold <- data[, .(state_ordinal)]
+    boot <- copy(data)
+    boot[, state := NULL]
+    boot <- cbind(boot[sample(nrow(boot)), ], hold)
+    boot <- boot[sample(nrow(boot), n, replace = TRUE), ]
+  }
+  K <- min(boot$state_ordinal)
+  boot[, A := stats$rbinom(n, 1, 0.5)]
+  if (effect_size != 0) {
+    boot[A == 1, state_ordinal := state_ordinal - 
+           round(rbeta4(.N, 0, 5, effect_size, 15, seed = seed), 0)]
+    boot[, state_ordinal := pmax(state_ordinal, K)]
+  }
+  boot[]
+}
+
 #' @export
-covid <- function() {
-  readRDS(here("data", "private", "covid-update.rds"))
+covid <- function(type = c("survival", "ordinal")) {
+  switch(match.arg(type), 
+         survival = readRDS(here("data", "private", "covid-update.rds")), 
+         ordinal = readRDS(here("data", "private", "covid_14day_ordinal.rds")))
 }
 
 #' @export
 truth <- function(data, type = c("survival", "binary", "ordinal"), ...) {
   args <- list(...)
   switch(match.arg(type), 
-         survival = tds(data, args$effect_size, args$horizon))
+         survival = tds(data, args$effect_size, args$horizon), 
+         ordinal = tdo(data, args$s1, args$s2))
 }
 
 tds <- function(data, effect_size, horizon) {
   mean(pmin(data$days + round(stats$rchisq(length(data$days), df = effect_size), 0), horizon) - 
          pmin(data$days, horizon))
+}
+
+tdo <- function(data, effect_size) {
+  states_1 <- pmax(data$state_ordinal - 
+                     round(rbeta4(nrow(data), 0, 5, effect_size, 15, sample(53443, 1)), 0))
+  states_0 <- data$state_ordinal
+  probs_1 <- vector("numeric", 6)
+  probs_0 <- probs_1
+  for (i in 0:5) {
+    probs_1[i + 1] <- mean(states_1 <= i)
+    probs_0[i + 1] <- mean(states_0 <= i)
+  }
+  
+  lo_1 <- vector("numeric", 5)
+  lo_0 <- lo_1
+  for (i in 1:5) {
+    lo_1[i] <- log(probs_1[i] / (1 - probs_1[i]))
+    lo_0[i] <- log(probs_0[i] / (1 - probs_0[i]))
+  }
+  mean(lo_1) - mean(lo_0)
 }
