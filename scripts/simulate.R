f45d06440a08359c635ab29a42f8cf931d53e9d7
Nick Williams <ntwilliams.personal@gmail.com>
2021-03-10 13:28:35 -0500

    updates.

--- scripts/simulate.R
+++ scripts/simulate.R
@@ -3,25 +3,23 @@
 # Department of Population Health Sciences 
 # Weill Cornell Medicine
 
-# .libPaths("/home/niw4001/R_local")
+.libPaths("/home/niw4001/R_local")
+
+setwd("/home/niw4001/covid-RCT-covar")
 
 box::use(./R/simulate, config[get], future[...])
 
 args <- commandArgs(trailingOnly = TRUE)
 config <- get(file = "./scripts/config.yml", config = args[1])
 
-id <- 1
-# id <- as.numeric(Sys.getenv("SLURM_ARRAY_TASK_ID"))
+id <- as.numeric(Sys.getenv("SLURM_ARRAY_TASK_ID"))
 op <- "./data/res"
-mch <- 1
-# mch <- config$machines
+mch <- config$machines
 
 tasks <- expand.grid(type = config$type, covar_id = seq_along(config$covar),
                      lasso = config$lasso, prog = config$prog, 
                      seed = sample(5346436, config$reps), 
                      n = config$nobs, effect_size = config$es, stringsAsFactors = FALSE)
-
-plan(multisession, workers = 8)
 
 simulate$partition(tasks, config$covar, id, mch, op)
 
--- scripts/simulate.sh
+++ scripts/simulate.sh
@@ -9,6 +9,6 @@ echo "$SLURM_ARRAY_TASK_ID"
 
 source ~/.bashrc
 spack load -r /bxc56dm
-Rscript simulate.R
+Rscript simulate.R ${1}
 
 exit 0
