d83c4b98d1d58f99f7503b946fc1931e5d6b6550
Nick Williams <ntwilliams.personal@gmail.com>
2021-04-02 16:49:18 -0400

    updating to toggle crossfitting and new algos.

--- scripts/simulate.R
+++ scripts/simulate.R
@@ -17,7 +17,7 @@ op <- "./data/res"
 mch <- config$machines
 
 tasks <- expand.grid(type = config$type, covar_id = seq_along(config$covar),
-                     algo = config$algo, prog = config$prog, 
+                     algo = config$algo, crossfit = config$crossfit, prog = config$prog, 
                      seed = sample(5346436, config$reps), 
                      n = config$nobs, effect_size = config$es, stringsAsFactors = FALSE)
 
