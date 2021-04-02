7aa6090bd6037258b0791eb36ba02b86d47128a4
Nick Williams <ntwilliams.personal@gmail.com>
2021-04-02 16:48:17 -0400

    scripts

--- R/simulate.R
+++ R/simulate.R
@@ -14,7 +14,8 @@ partition <- function(tasks, covar, id, machines, outpath) {
     }
     out[[r]] <- try(
         simulate(c19, tasks$type[row], covar[[tasks$covar_id[row]]], tasks$prog[row], tasks$seed[row], 
-                 algo = tasks$algo[row], n = tasks$n[row], effect_size = tasks$effect_size[row])
+                 algo = tasks$algo[row], crossfit = tasks$crossfit[row], 
+                 n = tasks$n[row], effect_size = tasks$effect_size[row])
       )
     
     if (!inherits(out[[r]], "try-error")) {
@@ -24,12 +25,14 @@ partition <- function(tasks, covar, id, machines, outpath) {
       out[[r]]["es"] <- tasks$effect_size[row]
       out[[r]]["progs"] <- tasks$prog[row]
       out[[r]]["algo"] <- tasks$algo[row]
+      out[[r]]["crossfit"] <- tasks$crossfit[row]
     }
   }
   saveRDS(out, 
           file.path(outpath, paste0(tasks$type[row], "_", 
                                     tasks$prog[row], "_",
                                     tasks$algo[row], "_",
+                                    tasks$crossfit[row], "_",
                                     id, ".rds")))
 }
 
@@ -44,17 +47,9 @@ simulate <- function(.data, type = c("survival", "ordinal"),
       covar <- NULL
     }
     f <- stats$as.formula(paste0("Surv(days, event) ~ ", paste(c("A", covar), collapse = " + ")))
-    surv <- suppressWarnings(survrct(f, "A", data = dat, estimator = "tmle", algo = args$algo))
+    surv <- suppressWarnings(survrct(f, "A", data = dat, estimator = "tmle", algo = args$algo, crossfit = args$crossfit))
     est_rmst <- rmst(surv, 14)
     est_sp <- survprob(surv, 7)
-    # if (args$lasso && estimator == "tmle") {
-    #   fits <- get_fits(surv)
-    #   out <- list(res = list(rmst = est_rmst, survprob = est_sp), 
-    #               hazard = as.matrix(stats$coef(fits$Hazard)), 
-    #               cens = as.matrix(stats$coef(fits$Censoring)), 
-    #               treatment = as.matrix(stats$coef(fits$Treatment)))
-    #   return(out)
-    # }
     return(list(rmst = est_rmst$estimates[[1]]$theta, 
                 rmst.std.error = est_rmst$estimates[[1]]$std.error,
                 survprob = est_sp$estimates[[1]]$theta, 
@@ -68,16 +63,9 @@ simulate <- function(.data, type = c("survival", "ordinal"),
     } else {
       f <- stats$as.formula(paste0("state_ordinal ~ ", paste(c("A", covar), collapse = " + ")))
     }
-    ord <- ordinalrct(f, target = "A", data = dat, estimator = "tmle", algo = args$algo)
+    ord <- ordinalrct(f, target = "A", data = dat, estimator = "tmle", algo = args$algo, crossfit = args$crossfit)
     est_lor <- log_or(ord)
     est_mw <- mannwhitney(ord)
-    # if (args$lasso) {
-    #   fits <- get_fits(ord)
-    #   out <- list(res = list(log_or = est_lor, mannwhit = est_mw), 
-    #               hazard = as.matrix(stats$coef(fits$Hazard)), 
-    #               treatment = as.matrix(stats$coef(fits$Treatment)))
-    #   return(out)
-    # }
     return(list(log_or = est_lor$estimates$lor, 
                 log_or.std.error = est_lor$estimates$std.error,
                 mannwhit = est_mw$estimates$theta, 
